e291f61d71d68e98eabc8df93ec6e0c6d1c13294
hkboss44 <hassankhaled4432@gmail.com>
2020-04-21 18:38:40 +0200

    mandy

---
+++ src/test/java/mandy/com/Hamoo/WWETesting.java
@@ -0,0 +1,22 @@
+package mandy.com.Hamoo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class WWETesting {
+
+	
+	@Test
+	public void Block1_Get() {
+		
+		//WWEWomenDivision W = new WWEWomenDivision();
+		String Champion = "Becky Lynch";
+		
+		assertEquals("Becky Lynch", Champion);
+		
+	}
+	
+	
+}
